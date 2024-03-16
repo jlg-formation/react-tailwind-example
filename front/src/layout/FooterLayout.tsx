0caf99f0d560bb2b0d5675d811d28688d23e3d39
Jean-Louis GUENEGO <jlguenego@gmail.com>
2024-03-16 14:47:01 +0100

    dev: added Link

--- front/src/layout/FooterLayout.tsx
+++ front/src/layout/FooterLayout.tsx
@@ -1,9 +1,11 @@
+import { Link } from "react-router-dom";
+
 function FooterLayout() {
   return (
     <footer className="flex h-12 bg-gray-100 px-2 items-center justify-center">
-      <a href="#" className=" items-center gap-2 flex hover:underline">
+      <Link to="/legal" className=" items-center gap-2 flex hover:underline">
         Mentions Légales
-      </a>
+      </Link>
     </footer>
   );
 }
--- front/src/layout/HeaderLayout.tsx
+++ front/src/layout/HeaderLayout.tsx
@@ -1,12 +1,13 @@
+import { Link } from "react-router-dom";
 import logoSvg from "/logo.svg";
 
 function HeaderLayout() {
   return (
     <header className="flex h-12 bg-gray-100 px-2">
-      <a href="#" className=" items-center gap-2 flex hover:underline">
+      <Link to="/" className=" items-center gap-2 flex hover:underline">
         <img src={logoSvg} alt="Logo" className="h-10" />
         <span className="font-bold text-xl">Gestion Stock</span>
-      </a>
+      </Link>
     </header>
   );
 }
