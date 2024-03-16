a054c39a1dd4128b8ef6ca193ddff51256581d2d
Jean-Louis GUENEGO <jlguenego@gmail.com>
2024-03-16 14:35:03 +0100

    dev: routing -> added conf in router

--- front/src/router.tsx
+++ front/src/router.tsx
@@ -1,11 +1,23 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "./App";
 import ErrorPage from "./layout/ErrorPage";
+import HomeView from "./views/HomeView";
+import LegalView from "./views/LegalView";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
     errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "",
+        element: <HomeView />,
+      },
+      {
+        path: "legal",
+        element: <LegalView />,
+      },
+    ],
   },
 ]);
