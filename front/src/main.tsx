a621412229c638862334959b5b7a763bddf7208a
Jean-Louis GUENEGO <jlguenego@gmail.com>
2024-03-16 14:46:23 +0100

    dev: bug in main.tsx -> remove the App.tsx component.

--- front/src/main.tsx
+++ front/src/main.tsx
@@ -1,13 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
 import { RouterProvider } from "react-router-dom";
+import "./index.css";
 import { router } from "./router.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <RouterProvider router={router} />
-    <App />
   </React.StrictMode>
 );
