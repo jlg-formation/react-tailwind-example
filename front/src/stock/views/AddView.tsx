de9f0cc8df5b2c1bb48da99ff7d8b273cff14c13
Jean-Louis GUENEGO <jlguenego@gmail.com>
2024-03-17 17:51:30 +0100

    dev: added react-hook-form

--- front/src/stock/views/AddView.tsx
+++ front/src/stock/views/AddView.tsx
@@ -1,25 +1,36 @@
 import { faPlus } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { FieldValues, useForm } from "react-hook-form";
 
 export default function AddView() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (newArticle: FieldValues) => {
+    console.log("newArticle: ", newArticle);
+  };
+
   return (
     <main>
       <h1>Ajouter un article</h1>
-      <form>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <label>
           <span>Nom</span>
-          <input type="text" />
-          <span className="error"></span>
+          <input type="text" {...register("name", { required: true })} />
+          <span className="error">{errors.name && "Champs requis."}</span>
         </label>
         <label>
           <span>Prix</span>
-          <input type="number" />
-          <span className="error"></span>
+          <input type="number" {...register("price", { required: true })} />
+          <span className="error">{errors.price && "Champs requis."}</span>
         </label>
         <label>
           <span>Quantité</span>
-          <input type="number" />
-          <span className="error"></span>
+          <input type="number" {...register("qty", { required: true })} />
+          <span className="error">{errors.qty && "Champs requis."}</span>
         </label>
         <div className="error"></div>
         <button className="primary">
