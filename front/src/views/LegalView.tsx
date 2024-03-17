3eba0c68190049e7acf174bfd95bf192fa22a98a
Jean-Louis GUENEGO <jlguenego@gmail.com>
2024-03-17 12:44:57 +0100

    dev: added legal view content

--- front/src/views/LegalView.tsx
+++ front/src/views/LegalView.tsx
@@ -1,5 +1,29 @@
 function LegalView() {
-  return <main>Legal view !</main>;
+  return (
+    <main>
+      <h1>Mentions Légales</h1>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Earum dolore
+        deserunt itaque doloremque facilis cumque enim optio blanditiis sequi!
+        Reprehenderit ex officiis optio aut, quasi maiores atque? Commodi,
+        temporibus odit!
+      </p>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veritatis
+        earum possimus quia placeat reprehenderit! Nostrum pariatur officiis
+        animi nobis doloribus dignissimos magni culpa, quidem ea veritatis ullam
+        maxime consequatur, aut laboriosam, omnis modi sunt. Nemo aliquam,
+        iusto, ab similique dolorem quas nostrum consectetur aliquid modi
+        blanditiis iste! Possimus, omnis. Vel eum quidem perferendis labore
+        dolorem deserunt incidunt eos maxime quae? Eius ut cupiditate animi
+        nobis sed natus dignissimos. Quam eligendi nemo voluptatem nihil eum ut,
+        aperiam autem tempore accusantium consequuntur! Iusto aliquam animi
+        rerum, vero ipsa iste a. Culpa minima quidem reiciendis eius deleniti
+        exercitationem molestias fugiat magni, suscipit tenetur.
+      </p>
+    </main>
+  );
 }
 
 export default LegalView;
